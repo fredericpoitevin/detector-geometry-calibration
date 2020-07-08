c619b8687eed6127beac892e22e302864b129c51
Frederic Poitevin <frederic.poitevin@gmail.com>
2020-07-08 14:55:27 -0700

    started defining commands - based on cryodrgn

---
+++ autosfx/__main__.py
@@ -0,0 +1,30 @@
+'''AutoSFX'''
+
+def main():
+    import argparse, os
+    parser = argparse.ArgumentParser(description=__doc__)
+    import autosfx
+
+    import autosfx.commands.setup
+    import autosfx.commands.powdersum
+    import autosfx.commands.centerfinder
+    import autosfx.commands.peakfinder
+    import autosfx.commands.indexer
+    import autosfx.commands.distoptimizer
+
+    subparsers = parser.add_subparsers(title='Choose a command')
+    subparsers.required = 'True'
+
+    def get_str_name(module):
+        return os.path.splitext(os.path.basename(module.__file__))[0]
+
+    for module in modules:
+        this_parser = subparsers.add_parser(get_str_name(module), description=module.__doc__)
+        module.add_args(this_parser)
+        this_parser.set_defaults(func=module.main)
+
+    args = parser.parse_args()
+    args.func(args)
+
+if __name__ == '__main__':
+    main()
---
+++ autosfx/expParams.py
@@ -0,0 +1,82 @@
+import numpy as np
+import os
+import psana
+
+# experiment parameters
+
+def expdet(experimentName,runNumber,detectorName):
+    try:
+        ds = psana.DataSource("exp="+experimentName+":run="+str(runNumber)+':idx')
+    except: 
+        return None,None
+
+    run = ds.runs().next()
+    env = ds.env()
+    times = run.times()
+    det = psana.Detector(detectorName)
+    epics = ds.env().epicsStore()
+    counter = 0
+    evt = None
+    while evt is None:
+        evt = run.event(times[counter])
+        counter += 1
+        if counter > 1000:
+            break
+        if counter > len(times)-2:
+            break
+    return evt,det
+
+class experiparams:
+
+    def __init__(self, experimentName, runNumber, detectorName=None, outDir=None):
+        
+        self.experimentName = experimentName        # cxic0415  
+        self.runNumber = runNumber                  # 98  
+        self.detectorName = detectorName            # self.parent.detAlias = self.getDetectorAlias(str(self.parent.detInfo))  
+        self.username = os.environ['USER']
+        self.paraDir = os.path.dirname(os.path.realpath(__file__))
+        self.outDir = outDir 
+        
+        self.setDefaultPath() 
+        
+
+
+    def setDefaultPath(self):
+        if self.outDir is None:
+            self.outDir = "/reg/data/ana03/scratch/autosfx/%s/%s"%(self.username,self.experimentName)
+        self.outDir = os.path.realpath(self.outDir)
+        self.cxiDir = self.outDir+'/r'+str(self.runNumber).zfill(4)
+        self.logDir = self.outDir+'/logs/r'+str(self.runNumber).zfill(4)
+        self.runDir = self.cxiDir
+
+
+    def setDefaultPsana(self):
+        try: 
+            ds = psana.DataSource("exp="+self.experimentName+":run="+str(self.runNumber)+':idx')
+        except: 
+            raise Exception('!!!!! Invalid experiment name or run number')
+
+        run = ds.runs().next()
+        env = ds.env()
+        times = run.times()
+        det = psana.Detector(self.detectorName)
+        epics = ds.env().epicsStore()
+        counter = 0
+        evt = None
+        while evt is None:
+            evt = run.event(times[counter])
+            counter += 1
+
+        
+        self.instrument = det.instrument()        
+        from experiment import myExp
+        exp = myExp(self.experimentName,self.runNumber,self.detectorName)
+        exp.Det 
+        self.clen = exp.clen or 0.0
+        self.clenEpics = exp.clenEpics
+        print(self.clen, self.clenEpics) 
+                 
+        self.detectorDistance = np.mean(det.coords_z(evt))*1.0e-6 
+        self.coffset = self.detectorDistance - self.clen 
+        self.pixelSize = det.pixel_size(run)*1.0e-6 
+
